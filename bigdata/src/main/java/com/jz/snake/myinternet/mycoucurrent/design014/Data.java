b6218a9bd9c0ffad4fc1d081b1805263dcd5dd76
jazzyshi1 <1091229807@qq.com>
2018-10-19 21:12:26 +0800

    提交

--- bigdata/src/main/java/com/jz/snake/myinternet/mycoucurrent/design014/Data.java
+++
@@ -1,7 +0,0 @@
-package com.jz.snake.myinternet.mycoucurrent.design014;
-
-public interface Data {
-
-	String getRequest();
-
-}
--- bigdata/src/main/java/com/jz/snake/myinternet/mycoucurrent/tetz.java
+++
@@ -1,7 +0,0 @@
-package com.jz.snake.myinternet.mycoucurrent;
-
-/**
- * Created by jazzyshi on 2018/9/2.
- */
-public class tetz {
-}
